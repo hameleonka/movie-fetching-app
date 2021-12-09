46810b5d3539c1955472fe75c3fc67a070011671
Natalia Milovanova <hameleonka28@gmail.com>
2021-12-08 21:23:39 -0500

    Initial commit with dummy data

---
+++ src/components/MoviesList.js
@@ -0,0 +1,20 @@
+import Movie from './Movie';
+
+import classes from './MoviesList.module.css';
+
+function MovieList(props) {
+  return (
+    <ul className={classes['movies-list']}>
+      {props.movies.map((movie) => (
+        <Movie
+          key={movie.id}
+          title={movie.title}
+          releaseDate={movie.releaseDate}
+          openingText={movie.openingText}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default MovieList;
